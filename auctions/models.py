dfeb6ceee74d0486203ea75a9a001b6315e7bd22
isabel <isargp@gmail.com>
2022-06-13 12:22:27 +0100

    style almost done!

--- auctions/models.py
+++ auctions/models.py
@@ -5,9 +5,11 @@ import datetime
 
 
 class Category(models.Model):
-   name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='pics/', null=True, blank=True)
+
    
-   def __str__(self):
+    def __str__(self):
        return self.name
 
 
